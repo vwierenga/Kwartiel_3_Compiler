4dc2b772d1ec9d7a0479eb46874419f379190961
Laptop-oud <jansenwilson@live.nl>
2017-02-16 15:18:10 +0100

    Created Checker and Type classes

---
+++ src/Checker.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Laptop-oud on 16-2-2017.
+ */
+public class Checker extends natoBaseVisitor<Type>{
+
+// override methods
+
+
+}
+
---
+++ src/Type.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Laptop-oud on 16-2-2017.
+ */
+public enum Type {
+    MESSAGE, CONFIRM, FALCON
+}
